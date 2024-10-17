e46cb42d59a27b668ea37bb57fe9bee0c92d29e7
s-waldo <stephen.waldo.94@gmail.com>
2024-10-17 15:52:12 -0500

    Finalize stage 1 tests and sudoku logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,47 +3,52 @@ class SudokuSolver {
     return row.toUpperCase().charCodeAt(0) - 65;
   }
 
+  convertStringToArray(puzzleString) {
+    let puzzleArray = [];
+    for (let i = 0; i < 9; i++) {
+      puzzleArray.push([]);
+      for (let j = 0; j < 9; j++) {
+        puzzleArray[i].push(puzzleString[i * 9 + j]);
+      }
+    }
+    return puzzleArray;
+  }
+
   validate(puzzleString) {
-    if (puzzleString.length !== 81) return false;
-    puzzleString.forEach((char) => {
+    if (puzzleString.length !== 81) {
+      return false;
+    }
+    for (const char of puzzleString) {
       let regex = /[0-9\.]/;
       if (!regex.test(char)) {
         return false;
       }
-    });
+    }
     return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleArray, row, column, value) {
     let testChar = isNaN(row) ? this.convertRowToDigit(row) : row;
-    let i = testChar * 9;
-    for (let j = 0; j < 9; j++) {
-      const testValue = puzzleString[i + j];
-      if (value == testValue) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  checkColPlacement(puzzleString, row, column, value) {
     for (let i = 0; i < 9; i++) {
-      const testValue = puzzleString[column + i * 9];
-      if (value == testValue) {
-        return false;
-      }
+      if (puzzleArray[testChar][i] == value) return false;
     }
     return true;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleArray, row, column, value) {
+    for (let i = 0; i < 9; i++) {
+      if (puzzleArray[i][column] == value) return false;
+    }
+    return true;
+  }
+
+  checkRegionPlacement(puzzleArray, row, column, value) {
     const rowDigit = isNaN(row) ? this.convertRowToDigit(row) : row;
     const rowStart = Math.floor(rowDigit / 3) * 3;
     const colStart = Math.floor(column / 3) * 3;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        const testValue = puzzleString[colStart + i * 9 + rowStart + j];
-        if (testValue == value) {
+        if (puzzleArray[rowStart + i][colStart + j] == value) {
           return false;
         }
       }
@@ -51,28 +56,35 @@ class SudokuSolver {
     return true;
   }
 
-  solve(puzzleString) {
-    while (puzzleString.includes(".")) {
-      for (let i = 0; i < 81; i++) {
-        if (puzzleString[i] === ".") {
-          const solutions = new Set();
-          for (let j = 1; j < 10; j++) {
-            if (this.checkRowPlacement(puzzleString, Math.floor(i / 9), i % 9, j) &&
-                this.checkColPlacement(puzzleString, Math.floor(i / 9), i % 9, j) &&
-                this.checkRegionPlacement(puzzleString, Math.floor(i / 9), i % 9, j)) {
-              solutions.add(j);
+  isValidGuess(puzzleArray, row, col, guess) {
+    if (
+      this.checkRowPlacement(puzzleArray, row, col, guess) &&
+      this.checkColPlacement(puzzleArray, row, col, guess) &&
+      this.checkRegionPlacement(puzzleArray, row, col, guess)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  solve(puzzleArray) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (puzzleArray[row][col] == ".") {
+          for (let guess = 1; guess <= 9; guess++) {
+            if (this.isValidGuess(puzzleArray, row, col, guess)) {
+              puzzleArray[row][col] = guess;
+              if (this.solve(puzzleArray)) {
+                return puzzleArray.map(el=> el.join('')).join('');
+              }
             }
           }
-          if (solutions.size === 1) {
-            puzzleString[i] = solutions.keys()[0];
-          }
-          if (solutions.size === 0) {
-            return false
-          }
+          puzzleArray[row][col] = ".";
+          return false;
         }
       }
     }
-    return puzzleString
+    return puzzleArray.map(el=> el.join('')).join('');
   }
 }
 
